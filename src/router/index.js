848e8810035c1eaadcec4b24fdb9eda24fe554b9
chikwongyip <527439024@qq.com>
2024-02-16 23:52:58 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,14 @@ const routes = [
     path: "/login",
     name: "Login",
     component: Login,
+    beforeEnter: (to, from, next) => {
+      const isLogin = localStorage.isLogin;
+      if (isLogin) {
+        next({ name: "Home" });
+      } else {
+        next();
+      }
+    },
   },
 ];
 
@@ -18,5 +26,13 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes,
 });
-
+router.beforeEach((to, from, next) => {
+  const isLogin = localStorage.isLogin;
+  if (isLogin || to.name === "Login") {
+    next();
+  } else {
+    console.log("未登录");
+    next({ name: "Login" }); // 跳转到登录页面
+  }
+});
 export default router;
