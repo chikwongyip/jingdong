d8d313e622fe3c9999e42810129d6474091c43b3
chikwongyip <527439024@qq.com>
2024-05-14 07:12:42 +0800

    search

--- src/views/shop/Shop.vue
+++ src/views/shop/Shop.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="wrapper">
+    <div class="search">
+      <div class="search__back iconfont">&#xe662;</div>
+      <div class="search__content">
+        <span class="search__content__icon"></span>
+        <span class="search__content_input"></span>
+      </div>
+    </div>
     <shop-info :item="item"></shop-info>
   </div>
 </template>
@@ -28,6 +35,36 @@ export default {
 </script>
 <style lang="scss" scoped>
 .wrapper {
-  padding: 0, 0.18rem;
+  padding: 0 0.2rem;
+}
+.search {
+  margin: 0.2rem 0 0.16rem 0;
+  display: flex;
+  &__back {
+    width: 0.3rem;
+    height: 0.32rem;
+    line-height: 0.32rem;
+    font-size: 0.25rem;
+    color: #b6b6b6;
+  }
+  &__content {
+    flex: 1;
+    line-height: 0.32rem;
+    border-radius: 16rem;
+    background: #f5f5f5;
+    display: flex;
+    &__icon {
+      width: 0.44rem;
+      height: 0.32rem;
+    }
+    &__input {
+      height: 0.32rem;
+      border: none;
+      outline: none;
+      background: none;
+      width: 100%;
+      padding-right: 0.2rem;
+    }
+  }
 }
 </style>
